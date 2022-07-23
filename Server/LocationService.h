f97f95c0f49fd833c7a1f1a1033a2fa442276dfb
chinhquoc01 <qhanpuoc01@gmail.com>
2022-07-24 01:59:16 +0700

    Tách module FavouriteLocation

--- Server/LocationService.h
+++ Server/LocationService.h
@@ -2,8 +2,8 @@
 
 vector<Location> get_all_location(string path);
 
-vector<Location> get_favourite_list(string path, string username);
 
-bool save_location(string path, string username, vector<string> locationIds);
+
+
 
 vector<Location> get_locations_by_type(string type, string path = "locations.json");
